1c126a6c1edb5ee63f9d0325771be95041c0453d
priyanka nishad <piyachaudhary50@gmail.com>
2025-02-11 12:24:51 +0530

    basic learning

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import { useState } from "react";
+
+import "./App.css";
+
+function App({ data }) {
+  const [show, setShow] = useState(false);
+  return (
+    <div className="container">
+      <div>
+        {data?.type === "folder" ? "📁" : "📄"}
+        <span onClick={() => setShow((prev) => !prev)}> {data?.name}</span>
+      </div>
+      {show &&
+        data?.children?.map((each, i) => {
+          return <App key={i} data={each} />;
+        })}
+    </div>
+  );
+}
+
+export default App;
